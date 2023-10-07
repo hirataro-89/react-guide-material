67397a49658b8dd45bcc9439a7c74ef82eded361
yutaro hirabayashi <hirataro.yu@gmail.com>
2023-10-07 18:22:59 +0900

    add 075_fragment

--- 04_react_basic/src/075_fragment/start/components/Child.js
+++ 04_react_basic/src/075_fragment/start/components/Child.js
@@ -1,11 +1,20 @@
 import "./Child.css";
+import React from "react";
 
 const Child = () => {
-  return (
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
-  );
+	return (
+		<React.Fragment key="">
+			<div className="component">
+				<h3>Hello Component</h3>
+			</div>
+			<h3>Hell fragment</h3>
+			<p>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium vitae nihil cum facere
+				animi alias commodi dolores aperiam maiores, quos provident incidunt repudiandae, natus id
+				saepe eius labore consequuntur ratione.
+			</p>
+		</React.Fragment>
+	);
 };
 
 export default Child;
