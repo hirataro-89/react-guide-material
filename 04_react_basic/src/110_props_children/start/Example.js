7c2ed076a81807a7d59a8034833220e79b470de2
yutaro hirabayashi <hirataro.yu@gmail.com>
2023-10-09 10:51:11 +0900

    add: 110_props_children

--- 04_react_basic/src/110_props_children/start/Example.js
+++ 04_react_basic/src/110_props_children/start/Example.js
@@ -2,14 +2,23 @@ import Profile from "./components/Profile";
 import Container from "./components/Container";
 
 const profile = [
-  { name: "Takashi", age: 19, country: "Japan", color: "green" },
-  { name: "Jane", age: 28, country: "UK", color: "blue" },
+  { name: "Takashi", age: 19, country: "Japan", color: "blue" },
+  { name: "Jane", age: 28, country: "UK", color: "red" },
 ];
 
 const Example = () => {
   return (
     <div>
-      <Container title="Childrenとは？" />
+      <Container title="Childrenとは？">
+        <Profile {...profile[0]} />
+        <Profile {...profile[1]} />
+      </Container>
+      <Container title='個別にわたす' children={
+        [
+          <Profile key={profile[0].name} {...profile[1]}/>,
+          <Profile key={profile[1].name} {...profile[0]}/>
+        ]
+      }/>
     </div>
   );
 };
