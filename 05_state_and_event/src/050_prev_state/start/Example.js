e7f902d3a22d349add0ab6cf7d294fdf8bf7130d
yutaro hirabayashi <hirataro.yu@gmail.com>
2023-10-10 18:57:44 +0900

    add: 050_prev_state

--- 05_state_and_event/src/050_prev_state/start/Example.js
+++ 05_state_and_event/src/050_prev_state/start/Example.js
@@ -1,7 +1,23 @@
 import { useState } from "react";
 
 const Example = () => {
-  
+  const [count, setConnt] = useState(0);
+  const countUp = () => {
+    setConnt(count + 1);
+    setConnt(prevstate => {
+      return prevstate + 1;
+    })
+  }
+  const countDown = () => {
+    setConnt(count - 1);
+  }
+  return (
+    <>
+      <p>現在のカウント数:{count}</p>
+      <button onClick={countUp}>+</button>
+      <button onClick={countDown}>-</button>
+    </>
+  )
 };
 
 export default Example;
