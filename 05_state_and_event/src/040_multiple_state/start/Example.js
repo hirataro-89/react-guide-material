fc9c3cad4691f2b4b8eff42f4adc8a83be14ebaa
yutaro hirabayashi <hirataro.yu@gmail.com>
2023-10-10 18:43:09 +0900

    add: 040_multiple_state

--- 05_state_and_event/src/040_multiple_state/start/Example.js
+++ 05_state_and_event/src/040_multiple_state/start/Example.js
@@ -1,5 +1,25 @@
+import { useState } from 'react';
+
 const Example = () => {
-  
+  const [ countA, setCountA ] = useState(0);
+  const [ countB, setCountB ] = useState(5);
+  const [ countC, setCountC ] = useState(13);
+  return (
+    <>
+      <p>ボタンを{countA}回押しました</p>
+      <button onClick={() => {
+        setCountA(countA + 1);
+      }}>countA</button>
+      <p>ボタンを{countB}回押しました</p>
+      <button onClick={() => {
+        setCountB(countB + 1);
+      }}>countB</button>
+      <p>ボタンを{countC}回押しました</p>
+      <button onClick={() => {
+        setCountC(countC + 1);
+      }}>countC</button>
+    </>
+  )
 };
 
 export default Example;
