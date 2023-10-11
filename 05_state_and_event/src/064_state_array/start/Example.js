9aef5a0c0e693faa1c085ba0cd700a0c3142099b
yutaro hirabayashi <hirataro.yu@gmail.com>
2023-10-11 20:07:23 +0900

    add: 064_state_array

--- 05_state_and_event/src/064_state_array/start/Example.js
+++ 05_state_and_event/src/064_state_array/start/Example.js
@@ -1,9 +1,19 @@
+import { useState } from "react";
+
 const Example = () => {
   const numArray = [1, 2, 3, 4, 5];
+  const [nums, setNums] = useState(numArray);
 
+  const shuffle = () => {
+    const newNums = [...nums];
+    const value = newNums.pop(); //一番最後の値を取得
+    newNums.unshift(value); //取得した値を配列の先頭に
+    setNums(newNums);
+  }
   return (
     <>
-      
+      <h1>{nums}</h1>
+      <button onClick={shuffle}>shuffle</button>
     </>
   );
 };
