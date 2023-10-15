bf74cedc59d39b2b8025eeefe617a51c08dc8fba
yutaro hirabayashi <hirataro.yu@gmail.com>
2023-10-15 19:26:27 +0900

    add: 010_list_components

--- 06_control_and_form/src/010_list_components/start/Example.js
+++ 06_control_and_form/src/010_list_components/start/Example.js
@@ -1,13 +1,14 @@
 
 const animals = ["Dog", "Cat", "Rat"];
 
-const Example = () => {
-
+const Example = ( ) => {
+  const animalList = animals.map((animal) =>
+    <li>Hello, {animal}</li>);
   return (
     <>
       <h3>配列の操作</h3>
       <ul>
-        
+        {animalList}
       </ul>
     </>
   );
