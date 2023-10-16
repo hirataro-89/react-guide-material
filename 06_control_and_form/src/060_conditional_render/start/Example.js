7d57b945f2820fc3c6e51eaf06202df533c27b91
yutaro hirabayashi <hirataro.yu@gmail.com>
2023-10-17 05:35:58 +0900

    add: 060_conditional_render

--- 06_control_and_form/src/060_conditional_render/start/Example.js
+++ 06_control_and_form/src/060_conditional_render/start/Example.js
@@ -1,30 +1,39 @@
 import { useState } from "react";
 
 const Example = () => {
-  const animals = ["Dog", "Cat", "Rat"];
+	const animals = ["Dog", "Cat", null, "Rat"];
 
-  const [filterVal, setFilterVal] = useState("");
+	const [filterVal, setFilterVal] = useState("");
 
-  return (
-    <>
-      <input
-        type="text"
-        value={filterVal}
-        onChange={(e) => setFilterVal(e.target.value)}
-      />
-      <ul>
-        {animals
-          .filter((animal) => {
-            const isMatch = animal.indexOf(filterVal) !== -1;
-            console.log(animal.indexOf(filterVal));
-            return isMatch;
-          })
-          .map((animal) => (
-            <li key={animal}>{animal}</li>
-          ))}
-      </ul>
-    </>
-  );
+	return (
+		<>
+			<input type="text" value={filterVal} onChange={(e) => setFilterVal(e.target.value)} />
+			<ul>
+				{animals
+					.filter((animal) => {
+            const animalStr = animal ?? "";
+						const isMatch = animalStr.indexOf(filterVal) !== -1;
+						console.log(animalStr.indexOf(filterVal));
+						return isMatch;
+					})
+					.map((animal) => {
+						/* if文 */
+						// if (animal === "Dog") {
+						// 	return <li key={animal}>{animal}★</li>;
+						// } else {
+						// 	return <li key={animal}>{animal}</li>;
+						// }
+
+						/* 三項演算子 */
+						// return <li key={animal} >{animal === "Dog" ? animal + "★" : animal}</li>;
+
+            /* &&, ?? */
+            return <li key={animal} >{animal ?? 'nullでした'}{animal === "Dog" && "★" }</li>; //trueの場合のみ★を表示
+
+					})}
+			</ul>
+		</>
+	);
 };
 
 export default Example;
