a96ad4b89ea4b77185d3126378c8e6e2f06fb97a
yutaro hirabayashi <hirataro.yu@gmail.com>
2023-10-17 07:49:46 +0900

    add: 110_multi_checkbox

--- 06_control_and_form/src/110_multi_checkbox/start/Example.js
+++ 06_control_and_form/src/110_multi_checkbox/start/Example.js
@@ -8,10 +8,53 @@ const Example = () => {
   ]);
 
   const [sum, setSum] = useState(0);
+  const handleChange = (e) => {
+    const newFruits = fruits.map(fruit => {
+      const newFruit = { ...fruit }; //新しい配列として複製したほうがよい
+      if (newFruit.label === e.target.value) {
+        newFruit.checked = !fruit.checked;
+      }
+      return newFruit;
+    })
+    setFruits(newFruits);
 
+    // let sumVal = 0;
+    // forEachを用いたやり方
+    // newFruits.forEach(fruit => {
+    //   if (fruit.checked) {
+    //     sumVal += fruit.value;
+    //   }
+    // })
+
+
+    // filterを用いたやり方
+    // newFruits
+    //   .filter((fruit) => fruit.checked)
+    //   .forEach((fruit) => (sumVal += fruit.value));
+
+    // reduceメソッドを用いたやり方
+     let sumVal = newFruits
+      .filter((fruit) => fruit.checked)
+      .reduce((sumVal, fruit) => sumVal + fruit.value, 0);
+
+    setSum(sumVal);
+  }
   return (
     <div>
-      
+      {fruits.map((fruit) => {
+        return (
+          <div key={fruit.label}>
+            <input
+              type="checkbox"
+              value={fruit.label}
+              id={fruit.label}
+              checked={fruit.checked}
+              onChange={handleChange}
+            />
+            <label htmlFor={fruit.label}>{fruit.label}:{fruit.value}</label>
+          </div>
+        )
+      })}
       <div>合計：{sum}</div>
     </div>
   );
