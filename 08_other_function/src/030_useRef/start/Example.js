b39a00845ae2ec506fdb57b6f32d1d1c1004d4c9
yutaro hirabayashi <hirataro.yu@gmail.com>
2023-10-22 18:22:57 +0900

    add: 030_useRef

--- 08_other_function/src/030_useRef/start/Example.js
+++ 08_other_function/src/030_useRef/start/Example.js
@@ -1,25 +1,48 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const Case1 = () => {
-  const [value, setValue] = useState("");
+	const [value, setValue] = useState("");
+	const inputRef = useRef();
 
-  return (
-    <div>
-      <h3>ユースケース1</h3>
-      <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
-      <button onClick>
-        インプット要素をフォーカスする
-      </button>
-    </div>
-  );
+	return (
+		<div>
+			<h3>ユースケース1</h3>
+			<input type="text" ref={inputRef} value={value} onChange={(e) => setValue(e.target.value)} />
+			<button onClick={() => inputRef.current.focus()}>インプット要素をフォーカスする</button>
+		</div>
+	);
+};
+
+const Case2 = () => {
+	const [playing, setPlaying] = useState(false);
+	const videoRef = useRef();
+	return (
+		<div>
+			<video src="./sample.mp4" style={{ maxWidth: "100%" }} ref={videoRef}></video>
+			<button
+				onClick={() => {
+					// if (playing) {
+					//   videoRef.current.pause();
+					// } else {
+					//   videoRef.current.play();
+					// }
+					playing ? videoRef.current.pause() : videoRef.current.play();
+					setPlaying((prev) => !prev);
+				}}
+			>
+				{playing ? "Stop" : "Play"}
+			</button>
+		</div>
+	);
 };
 
 const Example = () => {
-  return (
-    <>
-      <Case1 />
-    </>
-  );
+	return (
+		<>
+			<Case1 />
+			<Case2 />
+		</>
+	);
 };
 
 export default Example;
