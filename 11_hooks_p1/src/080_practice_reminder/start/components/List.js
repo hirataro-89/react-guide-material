f48b2754a4bcce5a43d1f065f1d9c4b87cac0fa3
yutaro hirabayashi <hirataro.yu@gmail.com>
2023-11-06 06:51:55 +0900

    add: 080_practice_reminder 1,2

--- 11_hooks_p1/src/080_practice_reminder/start/components/List.js
+++ 11_hooks_p1/src/080_practice_reminder/start/components/List.js
@@ -1,19 +1,15 @@
-const List = ({todos, deleteTodo}) => {
-    const complete = (id) => {
-        deleteTodo(id)
-    }
-    return (
-        <div>
-            {todos.map(todo => {
-                return (
-                    <div key={todo.id}>
-                        <button onClick={() => complete(todo.id)}>完了</button>
-                        <span>{todo.content}</span>
-                    </div>
-                )
-            })}
-        </div>
-    );
-}
+import Item from "./Item";
+const List = ({ todos, deleteTodo, updateTodo }) => {
+	const complete = (id) => {
+		deleteTodo(id);
+	};
+	return (
+		<div>
+			{todos.map((todo) => (
+				<Item key={todo.id} complete={complete} todo={todo} updateTodo={updateTodo} />
+			))}
+		</div>
+	);
+};
 
 export default List;
