9ecbf0460744afdc45af5f43a8e330060ed4ef9c
yutaro hirabayashi <hirataro.yu@gmail.com>
2023-12-03 21:50:08 +0900

    add: 010_redux_no_rtk

--- 13_redux/start/src/010_redux_no_rtk/components/CounterButton.js
+++ 13_redux/start/src/010_redux_no_rtk/components/CounterButton.js
@@ -1,8 +1,9 @@
-import { useCounterDispatch } from "../context/CounterContext";
+import { useDispatch } from "react-redux";
+// import { useCounterDispatch } from "../context/CounterContext";
 
 const CounterButton = ({calcType, step}) => {
-    
-    const dispatch = useCounterDispatch();
+    const dispatch = useDispatch();
+    // const dispatch = useCounterDispatch();
     
     const clickHandler = () => {
         dispatch({ type: calcType, step });
--- 13_redux/start/src/010_redux_no_rtk/context/CounterContext.js
+++ 13_redux/start/src/010_redux_no_rtk/context/CounterContext.js
@@ -3,32 +3,21 @@ import { createContext, useContext, useReducer } from "react";
 const CounterContext = createContext();
 const CounterDispatchContext = createContext();
 
-const CounterProvider = ({ children }) => {
-    const [state, dispatch] = useReducer((prev, { type, step }) => {
-        switch (type) {
-          case "+":
-            return prev + step;
-          case "-":
-            return prev - step;
-          default:
-            throw new Error('不明なactionです。')
-        }
-      }, 0);
-    return (
-        <CounterContext.Provider value={state}>
-            <CounterDispatchContext.Provider value={dispatch}>
-                {children}
-            </CounterDispatchContext.Provider>
-        </CounterContext.Provider>
-    )
-}
+const CounterProvider = ({ children, reducer }) => {
+	const [state, dispatch] = useReducer(reducer, 0);
+	return (
+		<CounterContext.Provider value={state}>
+			<CounterDispatchContext.Provider value={dispatch}>{children}</CounterDispatchContext.Provider>
+		</CounterContext.Provider>
+	);
+};
 
 const useCounter = () => {
-    return useContext(CounterContext);
-}
+	return useContext(CounterContext);
+};
 
 const useCounterDispatch = () => {
-    return useContext(CounterDispatchContext);
-}
+	return useContext(CounterDispatchContext);
+};
 
-export { CounterProvider, useCounter, useCounterDispatch }
+export { CounterProvider, useCounter, useCounterDispatch };
