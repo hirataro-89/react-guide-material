40bf9cd8fb2d49f02b0e5cc21a263e2ad1947c39
yutaro hirabayashi <hirataro.yu@gmail.com>
2023-12-05 07:25:12 +0900

    add: 030_redux_toolkit

--- 13_redux/start/src/030_redux_toolkit/store/modules/counter.js
+++ 13_redux/start/src/030_redux_toolkit/store/modules/counter.js
@@ -1,28 +1,34 @@
-const initialState = 0;
+import { createSlice } from "@reduxjs/toolkit";
 
-const reducer = (state = initialState, { type, payload }) => {
-  switch (type) {
-    case "counter/+":
+const counter = createSlice({
+  name: 'counter',
+  initialState: 0,
+  reducers: {
+    add(state, { type, payload }) {
+      console.log(type, payload);
       return state + payload;
-    case "counter/-":
+    },
+    minus(state, { type, payload }) {
+      console.log(type, payload);
       return state - payload;
-    default:
-      return state;
+    }
   }
-};
+})
 
-const add = (payload) => {
-    return {
-        type: "counter/+",
-        payload
-    }
-}
+// const initialState = 0;
 
-const minus = (payload) => {
-    return {
-        type: "counter/-",
-        payload
-    }
-}
+// const reducer = (state = initialState, { type, payload }) => {
+//   switch (type) {
+//     case "counter/+":
+//       return state + payload;
+//     case "counter/-":
+//       return state - payload;
+//     default:
+//       return state;
+//   }
+// };
 
-export { reducer, add, minus }
+const { add, minus } = counter.actions;
+
+export { add, minus }
+export default counter.reducer;
