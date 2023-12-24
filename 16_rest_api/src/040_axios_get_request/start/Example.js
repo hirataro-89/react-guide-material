c4491dbe3940c6882cc54083f14e42da27df7cc6
yutaro hirabayashi <hirataro.yu@gmail.com>
2023-12-25 07:45:54 +0900

    add: 040_axios_get_request

--- 16_rest_api/src/040_axios_get_request/start/Example.js
+++ 16_rest_api/src/040_axios_get_request/start/Example.js
@@ -1,5 +1,21 @@
+import {useEffect} from 'react';
+import axios from 'axios';
+
 const Example = () => {
-  return <></>;
+  useEffect(() => {
+    // パターン1 asyncとawaitを使用する
+    // ただ、useEffectの直下でasyncは使用できないので、
+    // 1つそれ用の関数を定義する必要がある。
+    const getTodo = async () => {
+      const res = await axios.get('http://localhost:3003/todo');
+      console.log(res.data);
+    }
+    getTodo();
+    // パターン2 .thenでつなげる
+    // axios.get('http://localhost:3003/todo').then((res) => {
+    //   console.log(res.data);
+    // });
+  })
 };
 
 export default Example;
