6da5f19ce38d3fc47913499830e7bc768158fb98
yutaro hirabayashi <hirataro.yu@gmail.com>
2023-12-25 15:42:56 +0900

    add: 指定されたパスの値をJSで取得

--- 17_nextjs_p1/start/src/pages/07_router/[name]/setting.js
+++ 17_nextjs_p1/start/src/pages/07_router/[name]/setting.js
@@ -1,5 +1,18 @@
-export default function setting() {
+import { useRouter } from 'next/router';
+
+export default function Setting({ query }) {
+  const router = useRouter();
+  console.log(router);
   return (
-    <h2>[name]/setting.js</h2>
+    <>
+      <h2>{query.name}</h2>
+      <h2>routerから取得：{router.query.name}</h2>
+    </>
   )
+}
+// SSRを行う際に使用する関数
+export async function getServerSideProps({ query }) {
+  return {
+    props: { query }
+  }
 }
