372c14f26e24a364c2a9b181fd8ba533d53eedc6
yutaro hirabayashi <hirataro.yu@gmail.com>
2023-12-25 16:23:54 +0900

    add: Link component

--- 17_nextjs_p1/start/src/pages/index.js
+++ 17_nextjs_p1/start/src/pages/index.js
@@ -1,9 +1,21 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <h2>HOME</h2>
+    <>
+      <h2>HOME</h2>
+      <Link href={{
+        pathname: '/07_router',
+        query: {
+          key: 'value'
+        }}}>
+        <a>/07_router</a>
+      </Link>
+    </>
   )
 }
+
+// Linkで囲った場合は画面リロードしない。
