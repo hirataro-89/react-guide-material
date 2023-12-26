ed01f76d204620885faf00fe1331ce2ca31871cb
yutaro hirabayashi <hirataro.yu@gmail.com>
2023-12-26 16:10:08 +0900

    add: コンポーネント間で情報を共有

---
+++ 17_nextjs_p1/start/src/pages/09_multipage_state/list.js
@@ -0,0 +1,8 @@
+import List from "../../components/list"
+import { useAppContext } from "../../context/AppContext"
+
+
+export default function PageList() {
+  const [list, ] = useAppContext();
+  return <List list={list} />
+}
