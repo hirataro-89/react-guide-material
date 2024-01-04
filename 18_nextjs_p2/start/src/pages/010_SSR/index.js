a65f5d47d901b6f7497ac25af92ceb6d2191c81c
yutaro hirabayashi <hirataro.yu@gmail.com>
2024-01-04 12:31:51 +0900

    add: SSRとは

--- 18_nextjs_p2/start/src/pages/010_SSR/index.js
+++ 18_nextjs_p2/start/src/pages/010_SSR/index.js
@@ -1,12 +1,14 @@
 import {useEffect, useState} from 'react';
 
-export default function SSR() {
+export default function SSR({message}) {
   const [state, setState] = useState('thankyou');
   console.log('hello');
+  console.log(message);
   const val = 0;
   useEffect(() => {
     console.log('useEffect');
     window.localStorage.setItem('key', 'value'); //windowオブジェクトはuseEffectの中で使う必要がある
+    document.cookie = 'value=0; path=/;'
   },[])
   return (
     <>
@@ -15,3 +17,16 @@ export default function SSR() {
     </>
   )
 };
+
+export async function getServerSideProps(context) {
+  const { cookie } = context.req.headers;
+  console.log('cookie:', cookie);
+  console.log('getServerSideProps executed');
+  return {
+    // redirect : {
+    //   destination: '/',
+    //   permanent: false //恒久的に処理を行うか否か true: permanent, false: temporary
+    // },
+    props: {message: 'From Server Side Props'}
+  }
+}
