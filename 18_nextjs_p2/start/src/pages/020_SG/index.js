3c9915e829b99f7489e75c2392cee55f58f5bc47
yutaro hirabayashi <hirataro.yu@gmail.com>
2024-01-11 09:27:34 +0900

    add: getStaticProps

--- 18_nextjs_p2/start/src/pages/020_SG/index.js
+++ 18_nextjs_p2/start/src/pages/020_SG/index.js
@@ -1,4 +1,10 @@
 // static site generation
-export default function IndexPage() {
-  return <h3>SG:</h3>
+export default function IndexPage({ message }) {
+  return <h3>SG: { message }</h3>;
 };
+
+export async function getStaticProps() {
+  return {
+    props: {message: 'From Static Props'}
+  }
+}
